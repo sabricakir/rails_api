037d99afd921e90bb62a871488cf1182a492b897
sabricakir <sabricakir86@gmail.com>
2023-09-01 18:23:37 +0300

    Add api requests tracking and limit

--- app/controllers/api/v1/authenticate_controller.rb
+++ app/controllers/api/v1/authenticate_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::AuthenticateController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_action :authenticate_user!
+  before_action :check_api_limit
+  before_action :log_api_request
 
   attr_reader :current_user, :current_api_token
 
@@ -16,6 +18,7 @@ class Api::V1::AuthenticateController < ActionController::Base
     authenticate_with_http_token do |token, _options|
       @current_api_token = ApiToken.where(active: true).find_by(token:)
       @current_user = @current_api_token&.user
+      @current_api_token&.update!(last_used_at: Time.zone.now)
     end
   end
 
@@ -25,5 +28,21 @@ class Api::V1::AuthenticateController < ActionController::Base
 
   def render_not_found_response
     render json: { error: 'Record not found' }, status: :not_found
+  end
+
+  def log_api_request
+    ApiRequest.create!(
+      user: current_user,
+      path: request.path,
+      method: request.method
+    )
+    response.set_header('Rails-API-Call-Limit',
+                        "#{current_user.api_requests_remaining}/#{User::MAX_API_REQUESTS_PER_30_DAYS}")
+  end
+
+  def check_api_limit
+    return unless current_user.api_limit_reached?
+
+    render json: { error: 'API limit reached' }, status: :too_many_requests
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_143035) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_151328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_requests", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "path", null: false
+    t.string "method", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_api_requests_on_user_id"
+  end
 
   create_table "api_tokens", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -20,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_143035) do
     t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_used_at"
     t.index ["user_id"], name: "index_api_tokens_on_user_id"
   end
 
@@ -60,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_143035) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "api_requests", "users"
   add_foreign_key "api_tokens", "users"
   add_foreign_key "email_verification_tokens", "users"
   add_foreign_key "password_reset_tokens", "users"
